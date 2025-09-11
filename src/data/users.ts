145e614ec2098f1ff015b651efbbb0688fad4b64
jamesnguyendev <jamesnguyen.devs@gmail.com>
2025-09-11 16:28:40 +0700

    chore: change infor user

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,9 +1,9 @@
 export const users = [
   {
     id: "1",
-    name: "Arham Khan",
-    username: "Aarhamkhnz",
-    email: "hello@arhamkhnz.com",
+    name: "Quản trị viên",
+    username: "Admin",
+    email: "admin@hello.com",
     avatar: "/avatars/arhamkhnz.png",
     role: "administrator",
   },
